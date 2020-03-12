9a5f18870335f84b243ca5a7db0099c3b63cf78a
Lasse Hunniche Klevang <lasse96-06@hotmail.com>
2020-03-12 11:47:43 +0100

    Update hypo.py

--- hypo.py
+++ hypo.py
@@ -57,24 +57,42 @@ def test_decode_inverts_encode(s):
 def test_builtin_reverse_function(some_list):
     #assert some_list.reverse() == my_reverse(some_list)
 
-    placeholder1 = some_list
-    placeholder2 = some_list
-    assert placeholder1.reverse() == placeholder2.reverse()
+    placeholder1 = some_list.copy()
+    placeholder2 = some_list.copy()
+    placeholder1.reverse()
+    placeholder2.reverse()
+    assert placeholder1 == placeholder2
 
-    placeholder3 = some_list
+    placeholder3 = some_list.copy()
     placeholder3.reverse()
-    print("" + placeholder3)
-    
-    assert placeholder3 != some_list
+    placeholder3.reverse()
+    assert placeholder3 == some_list
 
-    #placeholder3.reverse()
-    #assert placeholder3 == some_list
+@given(st.lists(st.integers(), min_size=1), st.lists(st.integers(), min_size=1))
+def test_builtin_reverse_function_again(list1, list2):
+    # joins both lists and reverses the result
+    # e.g. [1,2] and [3,4] becomes [1,2,3,4], and reversed to [4,3,2,1]
+    placeholder1 = list1.copy() + list2.copy()
+    placeholder1.reverse()
+
+    # reverses each list and glues the first one to the back of the second one
+    # [1,2] becomes [2,1], and [3,4] becomes [4,3]
+    # this becomes [4,3] + [2,1] = [4,3,2,1]
+    placeholder2 = list1.copy()
+    placeholder3 = list2.copy()
+    placeholder2.reverse()
+    placeholder3.reverse()
+    placeholder4 = placeholder3 + placeholder2
+
+
+    assert placeholder4 == placeholder1
+
+
 
 
 #test_decode_inverts_encode()
 test_builtin_reverse_function()
+test_builtin_reverse_function_again()
 
-#test_list = [0]
-#reversed_list = my_reverse(test_list)
-#print(reversed_list)
 
+#test_list = [0,1]
